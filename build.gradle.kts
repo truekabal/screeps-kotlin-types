fb7d8ba471383cd2924b712699c8ad8cc60bedde
Patrick Del Conte <patrick.delconte@umb.ch>
2019-06-10 17:30:52 +0200

    add structure interface as the supertype of energycontainer and Store

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ import com.moowork.gradle.node.task.NodeTask
 import org.jetbrains.kotlin.gradle.tasks.Kotlin2JsCompile
 
 plugins {
-    id("kotlin2js") version "1.3.30"
+    id("kotlin2js") version "1.3.31"
     `maven-publish`
     id("com.jfrog.bintray") version "1.8.1"
     id("com.moowork.node") version "1.2.0"
@@ -20,7 +20,7 @@ dependencies {
 }
 
 group = "ch.delconte.screeps-kotlin"
-version = "1.3.0"
+version = "1.4.0"
 
 val sourcesJar by tasks.registering(Jar::class) {
     classifier = "sources"
