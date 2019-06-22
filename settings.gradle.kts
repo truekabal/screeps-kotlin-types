99ece3debaa2f7107517e683047baf566b358db5
truekabal <truekabal@gmail.com>
2019-06-23 01:29:33 +0300

    project setup

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,11 @@
-rootProject.name = "screeps-kotlin-types"
+pluginManagement {
+    resolutionStrategy {
+        eachPlugin {
+            if (requested.id.id == "kotlin2js" || requested.id.id == "kotlin-dce-js") {
+                useModule("org.jetbrains.kotlin:kotlin-gradle-plugin:${requested.version}")
+            }
+        }
+    }
+}
+
+rootProject.name = "screeps_kotlin"
